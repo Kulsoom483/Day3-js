541ab7857dca70ce080adae924a2ec874c9b7cf7
Kulsoom483 <67598656+Kulsoom483@users.noreply.github.com>
2020-07-17 10:33:02 +0530

    Day3-assignment

---
+++ Day3ass2.js
@@ -0,0 +1,16 @@
+
+// Set the current grade of the student
+let grade = 85;
+
+// Check if grade is an A, B, C, D, or F
+if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else if (grade >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
